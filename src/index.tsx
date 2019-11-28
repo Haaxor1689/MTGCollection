9ccedf6a1e2823ba85724556a12f0a6def9d5bcf
Maros Betko <betkomaros@gmail.com>
2019-11-28 02:39:18 +0100

    Added theme providers and css baseline

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,22 @@
+import { CssBaseline } from "@material-ui/core";
+import { ThemeProvider as MUIThemeProvider, StylesProvider } from "@material-ui/core/styles";
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 import App from "./Components/App";
 import * as serviceWorker from "./serviceWorker";
+import { MainTheme } from "./Components/Styled/Theme";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <StylesProvider injectFirst>
+        <ThemeProvider theme={MainTheme}>
+            <MUIThemeProvider theme={MainTheme}>
+                <CssBaseline />
+                <App />
+            </MUIThemeProvider>
+        </ThemeProvider>
+    </StylesProvider>,
+    document.getElementById("root")
+);
 
 serviceWorker.register();
