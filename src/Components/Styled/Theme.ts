d560327a3e6a575a51bc86b29e67d7b0b0603445
Maros Betko <betkomaros@gmail.com>
2019-11-28 02:38:23 +0100

    Added theme and  typed styled components function wrappers

---
+++ src/Components/Styled/Theme.ts
@@ -0,0 +1,19 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import baseStyled, { css as styledCss, ThemedStyledInterface, ThemedStyledProps, ThemeProvider as ThemeProv } from "styled-components";
+
+const additionalArgs = {
+    constants: {
+        drawerWidth: "240px",
+    } 
+};
+
+export const MainTheme = createMuiTheme({
+}, additionalArgs);
+
+export const ThemeProvider = ThemeProv;
+export const css = styledCss;
+
+export type Theme = typeof MainTheme & typeof additionalArgs;
+export type ComponentProps<T> = ThemedStyledProps<T, Theme>;
+const styled = baseStyled as ThemedStyledInterface<Theme>;
+export default styled;
