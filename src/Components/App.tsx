9a08529021986c06e5e518116d94f45741eaa778
Maros Betko <betkomaros@gmail.com>
2019-11-28 02:41:19 +0100

    Added appbar and side drawer

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,11 +1,116 @@
+import { AppBar, Divider, Drawer, IconButton, List, ListItem, ListItemIcon, ListItemText, Toolbar, Typography } from "@material-ui/core";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import MailIcon from "@material-ui/icons/Mail";
+import MenuIcon from "@material-ui/icons/Menu";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
 import React from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
-import GoogleApi from "../Utility/GoogleApi";
+import { isNullOrUndefined } from "util";
+import { initialState, State } from "../State";
+import { reducer } from "../State/Reducers";
+import GoogleApi, { GoogleProfile } from "../Utility/GoogleApi";
 import Home from "./Home";
+import NotFound from "./NotFound";
 import SignIn from "./SignIn";
+import SignInButton from "./SignInButton";
+import { FlexCol } from "./Styled/Grid";
+import styled, { css, MainTheme } from "./Styled/Theme";
+
+const bodyOpen = css<any>`
+    margin-left: ${p => p.theme.constants.drawerWidth};
+    width: calc(100% - ${p => p.theme.constants.drawerWidth});
+    transition: ${p =>
+        p.theme.transitions.create(["width", "margin"], {
+            easing: p.theme.transitions.easing.sharp,
+            duration: p.theme.transitions.duration.enteringScreen,
+        })};
+`;
+
+const bodyClose = css<any>`
+    transition: ${p =>
+        p.theme.transitions.create(["width", "margin"], {
+            easing: p.theme.transitions.easing.sharp,
+            duration: p.theme.transitions.duration.leavingScreen,
+        })};
+`;
+
+const CustomAppBar = styled(AppBar)<{ open: boolean }>`
+    z-index: ${p => p.theme.zIndex.drawer + 1};
+    ${bodyClose}
+    ${p => p.open && bodyOpen}
+`;
+
+const MenuButton = styled(IconButton)<{ open: boolean }>`
+    margin-right: 36px;
+    ${p =>
+        p.open &&
+        css`
+            display: none;
+        `}
+`;
+
+const drawerOpen = css<any>`
+    width: ${p => p.theme.constants.drawerWidth};
+    transition: ${p =>
+        p.theme.transitions.create("width", {
+            easing: p.theme.transitions.easing.sharp,
+            duration: p.theme.transitions.duration.enteringScreen,
+        })};
+`;
+
+const drawerClose = css<any>`
+    transition: ${p =>
+        p.theme.transitions.create("width", {
+            easing: p.theme.transitions.easing.sharp,
+            duration: p.theme.transitions.duration.leavingScreen,
+        })};
+    overflow-x: hidden;
+    width: ${p => p.theme.spacing(7) + 1}px;
+`;
+
+const CustomDrawer = styled(Drawer).attrs(() => ({
+    variant: "permanent",
+}))<{ open?: boolean }>`
+    width: ${p => p.theme.constants.drawerWidth};
+    flex-shrink: 0;
+    white-space: nowrap;
+    ${p => (p.open ? drawerOpen : drawerClose)}
+
+    .MuiDrawer-paper {
+        ${p => (p.open ? drawerOpen : drawerClose)}
+    }
+`;
+
+const DrawerToolbar = styled(Toolbar)`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding: ${p => p.theme.spacing(0, 1)}px;
+`;
+
+const MainContent = styled.div<{ open: boolean }>`
+    flex-grow: 1;
+    padding: ${p => p.theme.spacing(3)}px;
+    ${bodyClose}
+    ${p => p.open && bodyOpen}
+`;
 
 const App: React.FC = () => {
-    const [signedIn, setSignedIn] = React.useState(false);
+    const [state, dispatch] = React.useReducer(reducer, initialState);
+
+    const [profile, setProfile] = React.useState<GoogleProfile>();
+    const [isSignedIn, setIsSignedIn] = React.useState(false);
+
+    const handleSignoutClick = () => {
+        GoogleApi.signOut();
+        setProfile(undefined);
+    };
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => setOpen(true);
+    const handleDrawerClose = () => setOpen(false);
 
     /**
      * Called at app init, sets Google API signin callback
@@ -13,18 +118,57 @@ const App: React.FC = () => {
      */
     React.useEffect(() => {
         GoogleApi.initClient(async (isSignedIn: boolean) => {
-            setSignedIn(isSignedIn);
+            setIsSignedIn(isSignedIn);
+            if (!isSignedIn) {
+                return;
+            }
+            setProfile(GoogleApi.getProfile());
+            GoogleApi.prepareAppData()(dispatch);
         });
     }, []);
 
     return (
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-            <Redirect to={signedIn ? "/" : "/signin/"} />
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/signin/" component={SignIn} />
-            </Switch>
-        </BrowserRouter>
+        <State.Provider value={[state, dispatch]}>
+            <CustomAppBar position="sticky" open={open}>
+                <Toolbar>
+                    <MenuButton color="inherit" aria-label="open drawer" onClick={handleDrawerOpen} edge="start" open={open}>
+                        <MenuIcon />
+                    </MenuButton>
+                    <Typography variant="h6">MTGCollection</Typography>
+                    <FlexCol />
+                    {isSignedIn ? (
+                        <>
+                            <button onClick={handleSignoutClick}>Sign Out</button>
+                            {!isNullOrUndefined(profile) && (
+                                <div>
+                                    <img src={profile.getImageUrl()} alt="Profile" />
+                                    <div>Welcome back, {profile.getGivenName()}</div>
+                                </div>
+                            )}
+                        </>
+                    ) : (
+                        <SignInButton onClick={GoogleApi.signIn} />
+                    )}
+                </Toolbar>
+            </CustomAppBar>
+            <CustomDrawer open={open}>
+                <DrawerToolbar>
+                    <IconButton onClick={handleDrawerClose}>{MainTheme.direction === "rtl" ? <ChevronRightIcon /> : <ChevronLeftIcon />}</IconButton>
+                </DrawerToolbar>
+                <Divider />
+                {/* {Drawer content} */}
+            </CustomDrawer>
+            <MainContent open={open}>
+                <BrowserRouter basename={process.env.PUBLIC_URL}>
+                    <Redirect to={isSignedIn ? "/" : "/signin/"} />
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/signin/" component={SignIn} />
+                        <Route component={NotFound} />
+                    </Switch>
+                </BrowserRouter>
+            </MainContent>
+        </State.Provider>
     );
 };
 
--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,21 +1,12 @@
 import React from "react";
-import { isNullOrUndefined } from "util";
-import { initialState, State } from "../State";
-import { reducer } from "../State/Reducers";
+import { State } from "../State";
 import CollectionParser from "../Utility/CollectionParser";
-import GoogleApi, { GoogleProfile } from "../Utility/GoogleApi";
+import GoogleApi from "../Utility/GoogleApi";
 import DeckPreview from "./DeckPreview";
 import { ImportCards } from "./ImportCards";
 
 const Home: React.FC = () => {
-    const [state, dispatch] = React.useReducer(reducer, initialState);
-
-    const [profile, setProfile] = React.useState<GoogleProfile>();
-
-    const handleSignoutClick = () => {
-        GoogleApi.signOut();
-        setProfile(undefined);
-    };
+    const [state, dispatch] = React.useContext(State);
 
     const importCollection = (value: string) => {
         GoogleApi.updateFile({ id: state.files.collection, fileContent: value });
@@ -26,24 +17,12 @@ const Home: React.FC = () => {
         console.log(await GoogleApi.getFileContents({ id: state.files.collection }));
     };
 
-    React.useEffect(() => {
-        setProfile(GoogleApi.getProfile());
-        GoogleApi.prepareAppData()(dispatch);
-    }, []);
-
     return (
-        <State.Provider value={[state, dispatch]}>
-            <button onClick={handleSignoutClick}>Sign Out</button>
-            {!isNullOrUndefined(profile) && (
-                <div>
-                    <img src={profile.getImageUrl()} alt="Profile" />
-                    <div>Welcome back, {profile.getGivenName()}</div>
-                </div>
-            )}
+        <>
             <ImportCards onImport={importCollection} />
             <button onClick={exportCollection}>Export collection</button>
             <DeckPreview deckName="collection" />
-        </State.Provider>
+        </>
     );
 };
 
