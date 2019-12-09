cd02b38781f346f94ab9c3a615d03a54d663ddfb
Maros Betko <betkomaros@gmail.com>
2019-12-09 13:36:59 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MTGCollection/precache-manifest.23318b848f256af9b4e94bc89d4ac787.js"
+  "/MTGCollection/precache-manifest.83a8ece628d626c6732112afd8059225.js"
 );
 
 self.addEventListener('message', (event) => {
