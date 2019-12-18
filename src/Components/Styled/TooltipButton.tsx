249fd5fa0c37d2fb7826cfe1a20d6edc92a58ca6
Maros Betko <betkomaros@gmail.com>
2019-12-18 22:22:34 +0100

    Added background collor option for tooltip button

--- src/Components/Styled/TooltipButton.tsx
+++ src/Components/Styled/TooltipButton.tsx
@@ -1,20 +1,37 @@
 import { IconButton, Tooltip, Typography } from "@material-ui/core";
 import { IconButtonProps } from "@material-ui/core/IconButton/IconButton";
 import React from "react";
+import styled, { css } from "./Theme";
+
+const StyledIconButton = styled(IconButton)<{ background?: "primary" | "secondary" }>`
+    ${p =>
+        p.background &&
+        css`
+            background-color: ${p.theme.palette[p.background].main};
+        `};
+    &:hover {
+        ${p =>
+        p.background &&
+            css`
+                background-color: ${p.theme.palette[p.background].dark};
+            `};
+    }
+`;
 
 type Props = {
     onClick: () => void;
     title: string;
     size?: IconButtonProps["size"];
+    background?: "primary" | "secondary";
 };
 
-const TooltipButton: React.FC<Props> = ({ onClick, title, size, children }) => {
+const TooltipButton: React.FC<Props> = ({ onClick, title, size, background, children }) => {
     return (
         <Tooltip title={title}>
-            <IconButton size={size} onClick={onClick} aria-label={title}>
+            <StyledIconButton size={size} background={background} onClick={onClick} aria-label={title}>
                 {children}
                 <Typography variant="srOnly">{title}</Typography>
-            </IconButton>
+            </StyledIconButton>
         </Tooltip>
     );
 };
