3f9f98118a540b149a3cb51bca387fb72e2f3c9e
Maros Betko <betkomaros@gmail.com>
2019-12-28 15:48:38 +0100

    Added selected state for drawer items

--- src/Components/DrawerDeckList.tsx
+++ src/Components/DrawerDeckList.tsx
@@ -5,6 +5,11 @@ import React from "react";
 import { Deck, DeckName, State } from "../State";
 import styled from "./Styled/Theme";
 
+const StyledAvatar = styled(Avatar)`
+    background-color: ${p => p.theme.palette.background.paper};
+    color: ${p => p.theme.palette.text.secondary};
+`;
+
 const StyledItemText = styled(ListItemText)`
     white-space: nowrap;
     overflow: hidden;
@@ -23,7 +28,7 @@ const DrawerDeckList: React.FC<Props> = ({ open }) => {
 
     const renderItem = (deck: Deck, name?: string, icon?: JSX.Element) => (
         <Tooltip key={deck.name} title={open ? "" : (state.decks[deck.name].isDirty ? "*" : "") + (name ?? deck.name)} placement="right">
-            <ListItem button onClick={selectDeck(deck.name)}>
+            <ListItem button onClick={selectDeck(deck.name)} selected={state.selectedDeck === deck.name}>
                 <ListItemAvatar>
                     <Badge
                         invisible={!state.decks[deck.name].isDirty}
@@ -35,9 +40,9 @@ const DrawerDeckList: React.FC<Props> = ({ open }) => {
                             horizontal: "right",
                         }}
                     >
-                        <Avatar alt={name ?? deck.name} src={deck.previewUrl}>
+                        <StyledAvatar alt={name ?? deck.name} src={deck.previewUrl}>
                             {icon ?? <CollectionsIcon />}
-                        </Avatar>
+                        </StyledAvatar>
                     </Badge>
                 </ListItemAvatar>
                 <StyledItemText primary={name ?? deck.name} />
