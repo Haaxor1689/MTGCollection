2f026f97ddea6da56e421216dc6a254944d8355f
Maros Betko <betkomaros@gmail.com>
2020-01-21 23:26:26 +0100

    Added NoSleep.js with types

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="react-scripts" />
+
+declare module "nosleep.js" {
+    class NoSleep {
+        enable: () => void;
+        disable: () => void;
+    }
+    export default NoSleep;
+}
