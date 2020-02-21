22f36b417234388bf6c59c27bb6e47b40ec1e80c
Haaxor1689 <betkomaros@gmail.com>
2020-02-21 18:07:09 +0100

    Updates

---
+++ precache-manifest.6e0fcc7de288c4bd08d204519ea0bfd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3efcc5c00ce9a15731ec1f6a5dccf7",
+    "url": "/MTGCollection/index.html"
+  },
+  {
+    "revision": "e603d584032459289e23",
+    "url": "/MTGCollection/static/js/2.075df5c7.chunk.js"
+  },
+  {
+    "revision": "97fbe82a36d0ccb4c1d1b78cbc9a55b5",
+    "url": "/MTGCollection/static/js/2.075df5c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d1c6ab8e433aba5e9dc",
+    "url": "/MTGCollection/static/js/main.e4b1ca59.chunk.js"
+  },
+  {
+    "revision": "bfa4a85bbf46ebe75eb1",
+    "url": "/MTGCollection/static/js/runtime-main.fb7e6c41.js"
+  },
+  {
+    "revision": "81aa9e7e65162b4a606bd91f7f552cd5",
+    "url": "/MTGCollection/static/media/logo.81aa9e7e.png"
+  },
+  {
+    "revision": "89ac5845527671d92b6aca070a42243e",
+    "url": "/MTGCollection/static/media/scryfallLogo.89ac5845.svg"
+  }
+]);
