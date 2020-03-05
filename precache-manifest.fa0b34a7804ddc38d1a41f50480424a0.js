d9ef55fb68c070e284e8bd54b880a38be14da42a
Maros Betko <betkomaros@gmail.com>
2020-03-05 22:20:42 +0100

    Updates

---
+++ precache-manifest.fa0b34a7804ddc38d1a41f50480424a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724fedfcf098fe37ddbe29968b0cf5b9",
+    "url": "/MTGCollection/index.html"
+  },
+  {
+    "revision": "3d99bb5cc8aaefc41109",
+    "url": "/MTGCollection/static/js/2.5158ebea.chunk.js"
+  },
+  {
+    "revision": "97fbe82a36d0ccb4c1d1b78cbc9a55b5",
+    "url": "/MTGCollection/static/js/2.5158ebea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c5966617d865a69ac53",
+    "url": "/MTGCollection/static/js/main.d9a370cf.chunk.js"
+  },
+  {
+    "revision": "bfa4a85bbf46ebe75eb1",
+    "url": "/MTGCollection/static/js/runtime-main.fb7e6c41.js"
+  },
+  {
+    "revision": "81aa9e7e65162b4a606bd91f7f552cd5",
+    "url": "/MTGCollection/static/media/logo.81aa9e7e.png"
+  },
+  {
+    "revision": "89ac5845527671d92b6aca070a42243e",
+    "url": "/MTGCollection/static/media/scryfallLogo.89ac5845.svg"
+  },
+  {
+    "revision": "93970fd8ba24f670e29c328dd1571ed6",
+    "url": "/MTGCollection/sw.js"
+  }
+]);
